58173bf6483e48389804fb356366effa0d7ac0cd
arpitmiracle <113773212+arpitmiracle@users.noreply.github.com>
2024-01-31 16:02:56 +0530

    update

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import http from "http";
 import cors from "cors";
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 const server = http.createServer(app);
 
--- route.js
+++ route.js
@@ -1,7 +1,7 @@
 import express from "express";
 const routes = express.Router();
-import authRoute from "./src/routes/auth.route";
-import userRoute from "./src/routes/user.route";
+import authRoute from "./src/routes/auth.route.js";
+import userRoute from "./src/routes/user.route.js";
 
 routes.use("/auth", authRoute);
 routes.use("/user", userRoute);
